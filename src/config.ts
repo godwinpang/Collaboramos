fee74fe7ea066c43720c7f35370aa37c2741f0dc
thomastangerine <thomastang.thomas97@gmail.com>
2019-06-03 17:17:40 -0700

    remove stuff

--- src/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyBj5RQyV6ZX8M_iNT8uNVb6XPXlQX5tlS8",
-    authDomain: "collaboramos.firebaseapp.com",
-    databaseURL: "https://collaboramos.firebaseio.com",
-    projectId: "collaboramos",
-    storageBucket: "collaboramos.appspot.com",
-    messagingSenderId: "262882989471"
-};
