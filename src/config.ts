256ce5e5dc1c638e82b327886674d4d2ace036d6
gvalliap <ganeshv757@gmail.com>
2019-06-08 23:37:47 -0700

    added config file

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCUajgfZkzTz6Z4xsR3nEeA6Skw-R_844A",
+  authDomain: "collaboramos-private.firebaseapp.com",
+  databaseURL: "https://collaboramos-private.firebaseio.com",
+  projectId: "collaboramos-private",
+  storageBucket: "collaboramos-private.appspot.com",
+  messagingSenderId: "1006372604779",
+  appId: "1:1006372604779:web:327d742f078983b3"
+};
